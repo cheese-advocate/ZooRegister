26fb551146671cd2465e0cc218b35d459550c23d
Tristan Ackermann <61502704+cheese-advocate@users.noreply.github.com>
2022-05-30 22:34:13 +0200

    Uploaded existing project files

---
+++ ZooRegisterClient/src/zooregisterclient/ServerCommunicationException.java
@@ -0,0 +1,14 @@
+/*
+ * Author:              Tristan Ackermann (FK6W29M15)
+ * Date:                2017-08-10
+ * NetBeans Version:    7.3.1
+ * Java Version:        1.8.0_141-b15
+ * File Description:    Exception class, thrown by the client when it is unable to properly communicate with the server.
+ */
+package zooregisterclient;
+
+/**
+ * Thrown when the client is unable to properly communicate with the server.
+ * @author FK6W29M15
+ */
+public class ServerCommunicationException extends Exception {}
